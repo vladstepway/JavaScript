6388705bded7fbd682d998f41a0f92d1d434a339
Vladislav <36796951+vladstepway@users.noreply.github.com>
2018-11-10 17:59:49 +0300

    lab5 - DOM & DHTML using

--- lab5/js/script.js
+++ lab5/js/script.js
@@ -5,26 +5,24 @@
      var maxLength = fromBox.getAttribute("maxlength");
      if (length == maxLength) {
          document.getElementById(toBox).focus();
-
-     }
-
- }
-
- function changeInput() {
-     var inputs, index;
-     inputs = document.getElementsByTagName('input');
-     increase += 15
-     for (index = 0; index < inputs.length; index++) {
-         inputs[index].style.width = increase + "px";
      }
  }
 
- function showMessage() {
-     document.getElementById("text1").innerHTML = "Заполните поле!";
-     document.getElementById("text2").innerHTML = "Заполните поле!";
-     document.getElementById("text3").innerHTML = "Заполните поле!";
- }
-
- function openModalWindow(){
-   
- }
+ // function changeInput() {
+ //     var inputs, index;
+ //     inputs = document.getElementsByTagName('input');
+ //     increase += 15
+ //     for (index = 0; index < inputs.length; index++) {
+ //         inputs[index].style.width = increase + "px";
+ //     }
+ // }
+ //
+ // function showMessage() {
+ //     document.getElementById("text1").innerHTML = "Заполните поле!";
+ //     document.getElementById("text2").innerHTML = "Заполните поле!";
+ //     document.getElementById("text3").innerHTML = "Заполните поле!";
+ // }
+ //
+ // function openModalWindow(){
+ //   
+ // }
